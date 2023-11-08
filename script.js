5d8647a122ae2ca16fb9506581dcb375ff765f44
Maliha Jamshaid <malihajamshaid17@gmail.com>
2023-11-08 22:04:25 +0500

    FINAL COMMIT

--- script.js
+++ script.js
@@ -4,41 +4,41 @@ $(function () {
     $("#recipes").on("click", ".btn-warning", handleUpdate);
     $("#addBtn").click(addRecipe);
     $("#updateSave").click(function () 
-  {
+{
     var id = $("#updateId").val();
     var title = $("#updateTitle").val();
     var body = $("#updateBody").val();
     $.ajax({
-      url: "https://usman-fake-api.herokuapp.com/api/recipes/" + id,
-      data: { title, body },
-      
-      method: "PUT",
-      success: function (response) {
+    url: "https://usman-fake-api.herokuapp.com/api/recipes/" + id,
+    data: { title, body },
+
+    method: "PUT",
+    success: function (response) {
         console.log(response);
         loadRecipies();
         $("#updateModal").modal("hide");
-      },
+    },
     });
-  });
+});
 });
 
 function addRecipe() 
 {
-  var title = $("#title").val();
-  var body = $("#body").val();
-  $.ajax({
+var title = $("#title").val();
+var body = $("#body").val();
+$.ajax({
     url: "https://usman-fake-api.herokuapp.com/api/recipes",
     method: "POST",
     data: { title, body },
     success: function (response)
     {
-      console.log(response);
-      $("#title").val("");
-      $("#body").val("");
-      loadRecipies();
-      $("#addModal").modal("hide");
+    console.log(response);
+    $("#title").val("");
+    $("#body").val("");
+    loadRecipies();
+    $("#addModal").modal("hide");
     },
-  });
+});
 }
 
 function handleDelete() 
@@ -61,47 +61,46 @@ $.ajax({
 
 function handleUpdate() 
 {
-  var btn = $(this);
-  var parentDiv = btn.closest(".recipe");
-  let id = parentDiv.attr("data-id");
-  $.get(
+var btn = $(this);
+var parentDiv = btn.closest(".recipe");
+let id = parentDiv.attr("data-id");
+$.get(
     "https://usman-fake-api.herokuapp.com/api/recipes/" + id,
     function (response) 
     {
-      $("#updateId").val(response._id);
-      $("#updateTitle").val(response.title);
-      $("#updateBody").val(response.body);
-      $("#updateModal").modal("show");
+    $("#updateId").val(response._id);
+    $("#updateTitle").val(response.title);
+    $("#updateBody").val(response.body);
+    $("#updateModal").modal("show");
     }
-  );
+);
 }
 
 function loadRecipies()
-  {
+{
     $.ajax({
     url: "https://usman-fake-api.herokuapp.com/api/recipes",
     method: "GET",
-  
-      error: function (response)
-      {
+
+    error: function (response)
+    {
         var recipes = $("#recipes");
         recipes.html("An Error has occured");
-      },
-      success: function (response)
-      {
+    },
+    success: function (response)
+    {
         console.log(response);
         var recipes = $("#recipes");
-  
+
         recipes.empty();
         for (var i = 0; i < response.length; i++) 
         {
-          var rec = response[i];
-          recipes.append(
+        var rec = response[i];
+        recipes.append(
             `<div class="recipe" data-id="${rec._id}"><h3>${rec.title}</h3><p><button class="btn btn-danger btn-sm float-right">delete</button><button class="btn btn-warning btn-sm float-right">Edit</button> ${rec.body}</p></div>`
-          );
+        );
         
         }
-      },
+    },
     });
-  }
-  
+}
